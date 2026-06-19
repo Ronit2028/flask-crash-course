effa05639fa8de533b820781e294bf9627aadcf5
Ronit2028 <Ronit@2525>
2026-06-19 12:33:46 +0530

    Code changed

--- app.py
+++ app.py
@@ -13,11 +13,21 @@ items_db = [
 # @app.route('/')
 # def welcome():
 #     pass
+@app.route('/')
+def home():
+    return "Welcome to Flask Crash Course!"
 
 # TODO 2: Define a GET route '/api/greet' that extracts query string parameter 'name'
 #         If 'name' is present, return JSON { "message": "Hello, <name>!" }
 #         If 'name' is missing, default to "Hello, Guest!"
 
+@app.route('/api/greet')
+def greetUser():
+    name = request.args.get("name")
+    if(name):
+        return jsonify({"message" : f"Hello, {name}"})
+    
+    return jsonify({"message" : f"Hello, Guest"})
 # TODO 3: Define a POST route '/api/items' that accepts a JSON body containing { "name": string }
 #         Parse payload using request.get_json()
 #         If "name" is missing, return error status 400
@@ -31,4 +41,6 @@ items_db = [
 # TODO 5: Start the server on port 5000 in debug mode if script is run directly
 
 if __name__ == '__main__':
+    
     print("Flask Server template is waiting for live coding...")
+    app.run(debug=True, port=5000)
